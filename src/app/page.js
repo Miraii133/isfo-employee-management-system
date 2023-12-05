58d7a9733a4909db34ebdb8e7ff8d411d852eccb
Mariel <marmabs692@gmail.com>
2023-12-05 16:05:12 +0800

    refactor: changed to props

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import EmployeeCard from "./components/EmployeeCard";
+import picture from "../../public/sample.jpg";
 
 export default function Home() {
   return (
     <main>
-      <EmployeeCard />
+      <EmployeeCard
+        picture={picture}
+        status="Active"
+        fullname="Mariel Mabano"
+        email="mgmabano@addu.edu.ph"
+        unit="Basic Education"
+        designation="Computer Science"
+      />
     </main>
   );
 }
